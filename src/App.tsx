b183b00d85633dae57585b8d014197c39047cef4
blaisebouda <blaisebouda05@gmail.com>
2025-10-13 12:09:55 +0000

    feat : installation of tailwinCss

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -17,11 +16,14 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className="bg-blue-50">
+        <button
+          className="bg-blue-100 text-white p-2 rounded"
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </button>
-        <p>
+        <p className="text-red-500">
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
@@ -29,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
